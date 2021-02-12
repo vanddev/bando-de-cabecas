996c83689e192be07ca2a6b406409aa64a40c968
Vanderson Alves <vanalvesdev@gmail.com>
2021-02-12 14:43:43 -0300

    started structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/scss/bootstrap.scss";
 import '../styles/globals.css'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,49 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import Image from 'next/image'
+import { Col, Container, Row } from 'react-bootstrap'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <Container className={styles.infoCommic}>
+          <Row noGutters>
+            <Col lg="4">
+              <Image
+                src="/ba-da-bacon.png"
+                alt="Picture of the author"
+                width={500}
+                height={500}
+                layout="responsive"
+              />
+            </Col>
+            <Col className={styles.info}>
+              <h1>BA-DA-BACON</h1>
+              <div className={styles.title}>Sinopse</div>
+              <div>Essa é a história de um cara chamado Gio Gianopoulos seguindo um mapa em busca de um Bacon Mágico. Um bacon tão delicioso e único que todo mundo quer encontrar. Pra conseguir, Gio vai ter que enfrentar chefs de cozinha, chapeiros descontrolados, cozinheiros falidos, motoqueiros selvagens, vendedores de trufa e capangas da indústria alimentícia. Tudo porque o pai dele não quer morrer comendo gelatina.</div>
+              <div className={styles.title}>Criação</div>
+              <div>Raoni Marqs</div>
+              <div className={styles.title}>Roteiro</div>
+              <div>Raoni Marqs</div>
+              <div className={styles.title}>Arte</div>
+              <div>Raoni Marqs</div>
+            </Col>
+          </Row>
+        </Container>
+
+      {/* <main className={styles.main}>
+        
         <div className={styles.infoCommic}>
           <Image
           src="/ba-da-bacon.png"
           alt="Picture of the author"
-          layout="fill"
+          width={300}
+          height={300}
         />
         </div>
         <h1 className={styles.title}>
@@ -56,9 +84,9 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </main>
+      </main> */}
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -67,7 +95,7 @@ export default function Home() {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
